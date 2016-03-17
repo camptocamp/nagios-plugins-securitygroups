5e09f51c920ae1afe488042d0e92c775e5d85da0
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2016-03-17 14:53:19 +0100

    renamed check and uses instances public_ips instead of sole EIP

--- public_ip.py
+++ public_ip.py
@@ -6,7 +6,7 @@ import boto3
 import sys
 import time
 
-class sg_eip:
+class sg_public:
     def __init__(self, args):
 
         self.__debug   = args.debug
@@ -20,7 +20,7 @@ class sg_eip:
                 region_name  = self.__region
                 )
 
-        self.__eips = []
+        self.__pub_ips= []
         self.__sg_eips = []
 
         self.out_status = 0
@@ -29,7 +29,7 @@ class sg_eip:
         self.__ec2_client = session.client('ec2')
 
         self.__get_security_groups()
-        self.__get_eips()
+        self.__get_instances()
         self.__check()
 
 
@@ -55,30 +55,39 @@ class sg_eip:
             for ip in item['IpRanges']:
                 self.__sg_eips.append(ip['CidrIp'].split('/')[0])
 
-    def __get_eips(self):
+    def __get_instances(self):
         '''
-        Get all elastic IP
+        Get all instances and fetch PublicIp if present
         '''
-        self.__print('Getting elastic IPs')
-        eips = self.__ec2_client.describe_addresses()
-        for eip in eips['Addresses']:
-            self.__eips.append(eip['PublicIp'])
+        self.__print('Getting all instances')
+        reservations = self.__ec2_client.describe_instances(
+                Filters=[
+                    {
+                        'Name': 'dns-name',
+                        'Values': ['ec2*'],
+                        }
+                    ]
+                )
+        for instances in reservations['Reservations']:
+            for instance in instances['Instances']:
+                self.__pub_ips.append(instance['PublicIpAddress'])
+
 
 
     def __check(self):
         '''
         Ensure no useless directory is present on the bucket
         '''
-        diff = list(set(self.__sg_eips) - set(self.__eips))
+        diff = list(set(self.__sg_eips) - set(self.__pub_ips))
         if (len(diff) != 0):
-            self.out_msg = 'Deprecated openings found'
+            self.out_msg = '%i Deprecated openings found' % len(diff)
             self.out_status = 2
             for i in diff:
                 self.__print(i)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Check if there are unused EIP opening in specified SG')
+    parser = argparse.ArgumentParser(description='Check if there are unused public IP opening in specified SG')
     parser.add_argument('--debug',  '-d',   help='Set verbosity level.', default=0, type=int)
     parser.add_argument('--profile', '-p', help='Pass AWS profile name.', default='default')
     parser.add_argument('--region', '-r',   help='Set AWS region.', default='eu-west-1')
@@ -87,6 +96,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    worker = sg_eip(args)
+    worker = sg_public(args)
     print worker.out_msg
     sys.exit(worker.out_status)
